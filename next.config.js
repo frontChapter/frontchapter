9be52f81bfe5585c2161541fb99b35954ee1f699
Saleh <ordinarysaleh@pm.me>
2025-09-01 17:54:38 +0330

    fix: update next.config.js for GitHub Pages deployment and adjust package.json scripts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,30 @@
 /** @type {import('next').NextConfig} */
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  // trim off `refs/heads` to get the branch name
+  const branch = process.env.GITHUB_REF
+    ? process.env.GITHUB_REF.split('/').pop()
+    : '';
+
+  // Set for GitHub Pages deployment
+  assetPrefix = branch === 'main' ? '/' : `/${branch}/`;
+  basePath = branch === 'main' ? '' : `/${branch}`;
+}
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  assetPrefix: '/frontchapter/',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix,
+  basePath,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
