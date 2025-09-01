04fe6512d2ca1ed7e100cca880c01edf245764c5
Saleh <ordinarysaleh@pm.me>
2025-09-01 15:30:43 +0330

    Prepare for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,14 @@
  * @type {import('next').NextConfig}
  */
 
+const repoName = 'frontchapter-website'; // Change if your repo name is different
+const isGithubPages = true;
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
 };
 
 module.exports = nextConfig;
