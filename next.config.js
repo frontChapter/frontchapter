a8d31071680c97e60d0720fa194ec7522c051571
Saleh <ordinarysaleh@pm.me>
2025-09-01 16:11:17 +0330

    refactor: comment out GitHub Pages configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
  * @type {import('next').NextConfig}
  */
 
-const repoName = 'frontchapter-website'; // Change if your repo name is different
-const isGithubPages = true;
+// const repoName = 'frontchapter-website'; // Change if your repo name is different
+// const isGithubPages = true;
 
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  // basePath: isGithubPages ? `/${repoName}` : '',
+  // assetPrefix: isGithubPages ? `/${repoName}/` : '',
 };
 
 module.exports = nextConfig;
